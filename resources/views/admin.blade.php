803ecf5f965ab031d5aa698cd9a9d9f2ead80ca0
rajeeb@unalus.com <rajeebkc22@gmail.com>
2018-12-13 14:05:36 +0900

    some changes made

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ダッシュボード</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                   admin　画面
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
